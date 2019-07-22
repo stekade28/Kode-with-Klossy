f35c7e6492043e8fa80330e533ed2cc9ba763158
Apple <apple@Apples-MacBook-Pro-6.local>
2019-07-22 10:56:47 -0700

    my first playground

---
+++ Day1KWKPlayground.playground/Contents.swift
@@ -0,0 +1,46 @@
+import UIKit
+
+var str = "Hello, playground"
+str
+
+var name = "Sharayu"
+name
+name = "Sharayu Tekade"
+name
+
+print("Hello World!")
+
+var age = "14"
+print(age)
+print("I am \(age)")
+print("I am \(name)")
+var first = "Karlie"
+var last = "Kloss"
+print("I am \(first)\(last)")
+print("I am \(last)\(first)")
+print("\(first) \(last)")
+
+var a = 12
+var b = 65
+var c = 31
+var d = 98
+(a + b + c + d)/4
+5<3
+7 == 7
+12 > 7
+6 != 8
+"karlie" == "karlie"
+"karlie" == " karlie"
+"Karlie" == "karlie"
+var luckyNum = 28
+var dogAge = 8
+if dogAge < 2 {print("You are a puppy")}
+else if dogAge > 12 {print("You are elderly")}
+else {print("You are also awesome")}
+
+var favoriteFood = "Fries"
+if favoriteFood == "Chipotle" {
+    print("You are healthy")
+}
+else if favoriteFood == "Starbucks" {print("You are hungry")}
+else {print("You are eating")}
