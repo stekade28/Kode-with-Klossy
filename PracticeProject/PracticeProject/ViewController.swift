3a83f74842adb5272c0b68bdeae212a770e1a70b
Apple <apple@Apples-MacBook-Pro-6.local>
2019-07-29 10:42:08 -0700

    Practice Day 6

---
+++ PracticeProject/PracticeProject/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  PracticeProject
+//
+//  Created by Apple on 7/29/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    var imagePicker = UIImagePickerController()
+    
+    @IBOutlet weak var newImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func takeSelfieTapped(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func chooseAnImageTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage { newImage.image = selectedImage}
+    imagePicker.dismiss(animated: true, completion: nil)
+    }
+
+}
+
