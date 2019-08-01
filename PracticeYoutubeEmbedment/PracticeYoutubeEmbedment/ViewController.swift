b6605cee213fcd9911a4ebaeea521756b204f7a4
Apple <apple@Apples-MacBook-Pro-6.local>
2019-08-01 16:49:05 -0700

    update

--- PracticeYoutubeEmbedment/PracticeYoutubeEmbedment/ViewController.swift
+++ PracticeYoutubeEmbedment/PracticeYoutubeEmbedment/ViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
-   
-    @IBOutlet weak var videoTapped: WKWebView!
+    @IBOutlet weak var video: WKWebView!
+    @IBOutlet weak var videoTwo: WKWebView!
+    @IBOutlet weak var videoThree: WKWebView!
+    @IBOutlet weak var videoFour: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         loadYoutube(videoID: "WwzihD_WmFE")
+        loadVideos(videoID: "HxZ6Xz73nGA")
+        loadVideoThree(videoID: "BPobdbmzY9o")
+        loadVideoFour(videoID: "J-05m7bboK0")
     }
+    
     func loadYoutube(videoID videoID:String) {
         guard
-            let youtubeURL = NSURL(string: "https:www.youtube.com/embeded/\(videoID)")
+            let youtubeURL = NSURL(string: "https:www.youtube.com/embed/\(videoID)")
             else {return}
-        wv.loadRequest( NSURLRequest(url: youtubeURL) )
+        video.load(URLRequest(url: youtubeURL as URL))
+    }
+    
+    func loadVideos(videoID videoID:String) {
+        guard
+            let youtubeURL = NSURL(string: "https:www.youtube.com/embed/\(videoID)")
+            else {return}
+        videoTwo.load(URLRequest(url: youtubeURL as URL))
 }
+    func loadVideoThree(videoID videoID:String) {
+        guard
+            let youtubeURl = NSURL(string: "https:www.youtube.com/embed/\(videoID)")
+            else {return}
+        videoThree.load(URLRequest(url: youtubeURl as URL))
+    }
+    func loadVideoFour(videoID videoID:String) {
+        guard
+            let youtubeURl = NSURL(string: "https:www.youtube.com/embed/\(videoID)")
+            else {return}
+        videoThree.load(URLRequest(url: youtubeURl as URL))
+    }
 }
